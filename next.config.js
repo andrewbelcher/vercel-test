8d41492f6eb4caa9da77d662c253d3b4549591d4
andrewbelcher <andrewbelcher@655282.no-reply.drupal.org>
2024-04-19 16:07:32 +0100

    Ignore errors

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextTranslate = require('next-translate-plugin')
 module.exports = nextTranslate({
   // https://nextjs.org/docs/app/api-reference/next-config-js/reactStrictMode
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 })
